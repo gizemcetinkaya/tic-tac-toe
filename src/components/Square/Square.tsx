cbbb86723d73b05ee18cc82cc11d32ab9518c02f
Gizem Selli <gizem.selli@matriksdata.com>
2022-12-27 22:29:13 +0100

    disabled props and css style have been added

--- src/components/Square/Square.tsx
+++ src/components/Square/Square.tsx
@@ -1,12 +1,13 @@
 
 type SquareProps = {
     value: string,
+    isDisabled: boolean,
     onClick: (e: React.MouseEvent<HTMLElement>) => void;
 }
 const Square = (props: SquareProps) => {
 
     return (
-        <div className="square" onClick={props.onClick}>{props.value}</div>
+        <div className={props.isDisabled ? 'square is-disabled' : 'square'} onClick={props.onClick}>{props.value}</div>
     )
 }
 
